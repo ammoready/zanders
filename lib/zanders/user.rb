ce4dcf5dc30653dcd47ec0e8cf10d477083513ff
David Knight <viacoffee@gmail.com>
2017-07-13 11:05:56 -0400

    User authentication

--- lib/zanders/user.rb
+++ lib/zanders/user.rb
@@ -1,16 +1,29 @@
 module Zanders
   class User < SoapClient
 
+    attr_reader :username, :password
+
     def initialize(options = {})
       requires!(options, :username, :password)
-      @options = options
+
+      @username = options[:username]
+      @password = options[:password]
     end
 
-    # TODO-david
-    #
-    # Waiting on an endpoint for this
     def authenticated?
-      true
+      response = soap_client(ITEM_API_URL).call(:login_check, message: build_request_data)
+      response = response.body[:login_check_response][:return][:item]
+
+      response.first[:value] == "0"
+    end
+
+    private
+
+    def build_request_data
+      {
+        username: @username,
+        password: @password
+      }
     end
 
   end
